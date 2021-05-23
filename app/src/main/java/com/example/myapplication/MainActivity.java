bb9adbc8c651d54f0548cb70170efd671a54f526
Austin <kimdy90919@gmail.com>
2021-05-23 22:14:38 +0900

    인트로 로딩화면 2초 후 맵 액티비티로 이동

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,26 +1,21 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button btn = findViewById(R.id.btn_to_map);
-        btn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                Intent intent = new Intent(MainActivity.this, Map_activity.class);
-                startActivity(intent);
-            }
-        });
-    }
 
+        try{
+            Thread.sleep(2000); //wait per millis
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        startActivity(new Intent(this, Map_activity.class));
+        finish();
+    }
 }
